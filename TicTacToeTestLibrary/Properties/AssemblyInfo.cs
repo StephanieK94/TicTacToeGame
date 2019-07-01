6a46c51b11983f9e1ed1f7dcb4b78804a87649b4
StephanieK <stephanie.kitchen@serko.com>
2019-07-02 11:24:53 +1200

    First commit setting up file with test project

---
+++ TicTacToeTestLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TicTacToeTestLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TicTacToeTestLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("def26eb3-3803-45a8-8250-19b4fe092ebf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TicTacToeTestLibrary/TicTacTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToeTestLibrary
+{
+    [TestClass]
+    public class TicTacTests
+    {
+        [TestMethod]
+        public void InitialTest()
+        {
+            
+        }
+    }
+}
