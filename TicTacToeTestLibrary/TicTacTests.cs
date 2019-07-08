7fe3a9419e0341fb59ede0eb20c93c3787af4ae0
StephanieK <stephanie.kitchen@serko.com>
2019-07-08 22:19:08 +1200

    Removal of now redundant first test

--- TicTacToeTestLibrary/TicTacTests.cs
+++ TicTacToeTestLibrary/TicTacTests.cs
@@ -11,17 +11,6 @@ namespace TicTacToeTestLibrary
     public class TicTacTests
     {
         [Test]
-        public void GivenNewGame_ReturnsCharArray()
-        {
-            var game = new TicTacToeGame.Game();
-
-            var newGame = game.NewGame();
-            var expected = new char[3, 3];
-
-            Assert.AreEqual(expected, newGame);
-        }
-
-        [Test]
         public void GivenNewGame_ReturnsGameBoardLayout()
         {
             var game = new TicTacToeGame.Game();
