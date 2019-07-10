7a3228884c61e21568884f3d8bcd888db0f5e9de
StephanieK <stephanie.kitchen@serko.com>
2019-07-11 01:48:56 +1200

    Passing tests for winning row and column in first row and column

---
+++ TicTacToeGame/WinnerCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    public class WinnerCalculator
+    {
+        public bool CalculateIfWinner(Token[,] currentBoard)
+        {
+            if (currentBoard[0,0].Equals(currentBoard[1, 0]) && currentBoard[0, 0].Equals(currentBoard[2,0])) return true;
+            if (currentBoard[0,0].Equals(currentBoard[0, 1]) && currentBoard[0, 0].Equals(currentBoard[0,2])) return true;
+            else return false;
+        }
+
+    }
+}
---
+++ TicTacToeTestLibrary/WinnerTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeGame;
+
+namespace TicTacToeTestLibrary
+{
+    public class WinnerTests
+    {
+        [TestFixture]
+        public class WinningTests
+        {
+            [Test]
+            public void GivenColumn_ReturnWinner()
+            {
+                var rowWinner = new Token[,] 
+                { 
+                    { Token.X, Token.Empty, Token.Empty }, 
+                    { Token.X, Token.Empty, Token.Empty }, 
+                    { Token.X, Token.Empty, Token.Empty }
+                };
+
+                var winnerCalculator = new WinnerCalculator();
+                var isWinner = winnerCalculator.CalculateIfWinner(rowWinner);
+
+                Assert.True(isWinner);
+            }
+
+            [Test]
+            public void GivenRow_ReturnWinner()
+            {
+                var colWinner = new Token[,]
+                {
+                    { Token.X, Token.X, Token.X },
+                    { Token.Empty, Token.Empty, Token.Empty },
+                    { Token.Empty, Token.Empty, Token.Empty }
+                };
+
+                var winnerCalculator = new WinnerCalculator();
+                var isWinner = winnerCalculator.CalculateIfWinner(colWinner);
+
+                Assert.True(isWinner);
+            }
+        }
+    }
+}
