e1e75619a673f0684531e9a60a406e97c1b7c24c
StephanieK <stephanie.kitchen@serko.com>
2019-07-16 14:13:37 +1200

    Removal of unnessessary code from WinnerCalculator

--- TicTacToeGame/WinnerCalculator.cs
+++ TicTacToeGame/WinnerCalculator.cs
@@ -33,42 +33,5 @@ namespace TicTacToeGame
 
             IsWinner = (WinRow || WinColumn || WinDiagonal || WinRevDiagonal);
         }
-
-
-        /*public bool CalculateIfWinner(Player player, Token[,] currentBoard)
-        {
-            if (IsRowWinner(player, currentBoard) == false && IsColumnWinner(player, currentBoard) == false 
-                && IsDiagonlWinner(player, currentBoard) == false) return false;
-            return true;
-        }
-
-        public bool IsRowWinner(Player player, Token[,] currentBoard)
-        {
-            for(var row =0; row<=2; row++)
-            {
-                if (currentBoard[row, 0] == player.Position && currentBoard[row, 1] == player.Position &&currentBoard[row, 2] == player.Position)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool IsColumnWinner(Player player, Token[,] currentBoard)
-        {
-            for (var col = 0; col <= 2; col++)
-            {
-                if (currentBoard[0,col] == player.Position && currentBoard[1,col] == player.Position && currentBoard[2,col] == player.Position)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool IsDiagonlWinner(Player player, Token[,] currentBoard)
-        {
-            if ((currentBoard[0,0] == player.Position &&  currentBoard[1,1] == player.Position &&currentBoard[2,2] == player.Position) || (currentBoard[0,2] == player.Position && currentBoard[1,1] == player.Position && currentBoard[2,0] == player.Position)) 
-            {
-                return true;
-            }
-            else return false;
-        }*/
     }
 }
